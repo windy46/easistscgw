a046ea337f38f0ed9c33e9c6c8e51c029d0d2f49
windy46 <842860890@qq.com>
2024-11-07 02:22:56 +0800

    easistscgw init

---
+++ src/main/java/org/simpleway/springprogram/FallbackController.java
@@ -0,0 +1,13 @@
+package org.simpleway.springprogram;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback")
+    public String fallback() {
+        return "Fallback response due to delay or circuit breaker";
+    }
+}
